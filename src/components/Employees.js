6bdeb2dd611679d13931d5b76f864d131c6bebc4
hu-al <barroscuro@gmail.com>
2022-01-03 19:18:54 -0600

    fake logout

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -1,5 +1,101 @@
+import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
+import styles from "./Employees.module.css";
+
 const Employees = () => {
-  return <div>Employees</div>;
+  const [form, setForm] = useState({ name: "", lastName: "", date: "" });
+  const [errors, setErrors] = useState({});
+
+  const setField = (field, value) => {
+    setForm({
+      ...form,
+      [field]: value,
+    });
+
+    if (!!errors[field])
+      setErrors({
+        ...errors,
+        [field]: null,
+      });
+  };
+
+  const findFormErrors = () => {
+    const { name, lastName, date } = form;
+    const newErrors = {};
+
+    if (!name || name === "") newErrors.name = "El nombre es necesario";
+    if (name.length > 30)
+      newErrors.name = "El nombre no puede tener más de 30 caracteres";
+    if (!lastName || lastName === "")
+      newErrors.lastName = "El apellido es necesario";
+    if (lastName.length > 30)
+      newErrors.lastName = "El apellido no puede tener más de 30 caracteres";
+    if (!date || date === "") newErrors.date = "La fecha es necesaria";
+
+    return newErrors;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newErrors = findFormErrors();
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+    } else {
+      console.log("submit correcto", form);
+      //sendEmployee(form);
+    }
+  };
+  return (
+    <div className={styles.formEmployee}>
+      <Form noValidate onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            maxLength={30}
+            onChange={(e) => {
+              setField("name", e.target.value);
+            }}
+            isInvalid={!!errors.name}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.name}
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Apellidos</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            maxLength={30}
+            onChange={(e) => {
+              setField("lastName", e.target.value);
+            }}
+            isInvalid={!!errors.lastName}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.lastName}
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Fecha de nacimiento</Form.Label>
+          <Form.Control
+            required
+            type="date"
+            onChange={(e) => {
+              setField("date", e.target.value);
+            }}
+            isInvalid={!!errors.date}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.date}
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </div>
+  );
 };
 
 export default Employees;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,45 @@
 import { findNonSerializableValue } from "@reduxjs/toolkit";
-import { Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { fakeLogout } from "../actions/fakeLogin";
 
 const Header = () => {
   let isLogged = useSelector((state) => state.fakeLogin.isLogged);
+  let dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(fakeLogout());
+  };
 
   return (
     <div>
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand>Prueba</Navbar.Brand>
-        <Nav>
-          <Nav.Link as={Link} to="/">
-            Home
-          </Nav.Link>
-          <Nav.Link as={Link} to="employees">
-            Employees
-          </Nav.Link>
-          <Nav.Link as={Link} to="upload">
-            Upload
-          </Nav.Link>
-          {isLogged ? (
-            <Nav.Link as={Link} to="logout">
-              Logout
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Home
             </Nav.Link>
-          ) : (
-            <Nav.Link as={Link} to="login">
-              Login
+            <Nav.Link as={Link} to="employees">
+              Employees
             </Nav.Link>
-          )}
-        </Nav>
+            <Nav.Link as={Link} to="upload">
+              Upload
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            {isLogged ? (
+              <Nav.Link as={Link} onClick={handleClick} to="/">
+                Logout
+              </Nav.Link>
+            ) : (
+              <Nav.Link as={Link} to="login">
+                Login
+              </Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
